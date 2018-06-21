a93cbe87ab6da7ab67bccac9d2dddd6f2627471a
Teste <Teste@COREI5PC>
2018-06-21 20:12:57 -0300

    Add project files.

---
+++ giftstore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(giftstore.Startup))]
+namespace giftstore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
